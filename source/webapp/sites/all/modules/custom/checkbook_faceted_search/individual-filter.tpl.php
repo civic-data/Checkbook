3d03063b5e113e519b6252904019ec84451f9add
Saroja Gade <saru855@gmail.com>
2013-11-11 11:12:57 -0500

    Fixed the issue with auto population on details pages.

--- source/webapp/sites/all/modules/custom/checkbook_faceted_search/individual-filter.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_faceted_search/individual-filter.tpl.php
@@ -52,6 +52,8 @@ else{
   <div class="checked-items">
     <?php    
     foreach ($checked as $row) {
+      $row[0] = str_replace('__','/', $row[0]);
+      $row[1] = str_replace('__','/', $row[1]);
       echo '<div class="row">';
       echo '<div class="checkbox"><input class="styled" name="' . $autocomplete_id . '" type="checkbox" checked="checked" value="' . urlencode($row[0]) . '" onClick="return applyTableListFilters();"></div>';
       echo '<div class="name">' . _break_text_custom2($row[1],15) . '</div>';
@@ -64,6 +66,8 @@ else{
     <div class="rows">
     <?php
     foreach ($unchecked as $row) {
+      $row[0] = str_replace('__','/', $row[0]);
+      $row[1] = str_replace('__','/', $row[1]);
       echo '<div class="row">';
       echo '<div class="checkbox"><input class="styled" name="' . $autocomplete_id . '" type="checkbox" '  .  $disabled .  'value="' . urlencode($row[0]) . '" onClick="return applyTableListFilters();"></div>';
       echo '<div class="name">' . _break_text_custom2($row[1],15) . '</div>';
