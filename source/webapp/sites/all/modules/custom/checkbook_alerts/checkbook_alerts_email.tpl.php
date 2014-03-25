a07cdb579a761d8cafb26be063f79e2285709820
Jeremy Styers <jeremy.styers@reisystems.com>
2014-03-24 22:41:08 -0400

    Fixed multiple issues
    Fixed spacing issue on alerts pages

--- source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
@@ -6,12 +6,12 @@ $message = "Below are the results of the triggered alert(s) from checkbooknyc.co
 foreach($alerts as $alert)
 {
     $day = $alert-> minimum_days  == 1 ?  "Every Day" :  "Every " . $alert->minimum_days . " days";
-    $message .= "Alert description : " . $alert->label . PHP_EOL;
-    $message .= "Alert frequency: " . $day  . PHP_EOL;
-    $message .= "Number of new records that match alert criteria: " . $alert->new_count. "
+$message .= "Alert description : " . $alert->label . PHP_EOL;
+$message .= "Alert frequency: " . $day  . PHP_EOL;
+$message .= "Number of new records that match alert criteria: " . $alert->new_count. "
 Link to Checkbook NYC alert results: ". $alert->user_url. "
 To unsubscribe to this alert click here: " . $conf['check_book']['data_feeds']['site_url'].(substr($conf['check_book']['data_feeds']['site_url'],-1)=="/"?'':"/")."alert/unsubscribe/".$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient).
-        "
+    "
 
 
     ";
